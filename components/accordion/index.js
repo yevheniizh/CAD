28939bb2e396fd8b3238e488a21837d16b21010c
yevhenii-zhyrov <yevhenii.zhyrov@authenticpartners.live>
2022-06-05 23:01:17 +0300

    refactor: Delegate expand-collapse accordion to all nav-area buttons

--- components/accordion/index.js
+++ components/accordion/index.js
@@ -1,21 +1,23 @@
-import App from '../../js/index.js';
+import App from "../../js/index.js";
 
-const elements = document.querySelectorAll(".accordion");
+const navArea = document.querySelector(".nav-area");
 
-elements.forEach( element =>
-  element.addEventListener("click", () => {
-    element.classList.toggle("active");
-    const panel = element.nextElementSibling;
+// Delegate to expand/collapse accordion to all nav-area buttons
+navArea.addEventListener("click", ( e ) => {
+  const navButton = e.target.closest(".accordion-button");
+
+  if (navButton) {
+    navButton.classList.toggle("active");
+    const panel = navButton.nextElementSibling;
 
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
     } 
-  } )
-);
+  }
+} )
 
 // Wireframe button
 const button = document.querySelector(".button.wireframe");
-
 button.addEventListener("click", () => App.setWireframe());
